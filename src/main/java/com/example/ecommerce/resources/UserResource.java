9ace9e3198b33360b2dcd8822b15f2b57fea856f
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-19 15:42:22 -0300

    feat: User entity and resource

---
+++ src/main/java/com/example/ecommerce/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joao", "joao@gmail.com", "999999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
