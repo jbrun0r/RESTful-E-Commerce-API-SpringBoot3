5578a434db885ef46b48b34a3cfe543bf35cb14f
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-20 12:39:04 -0300

    feat: Order

--- src/main/java/com/example/ecommerce/config/TestConfig.java
+++ src/main/java/com/example/ecommerce/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.config;
 
+import java.time.Instant;import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.ecommerce.entities.Order;
 import com.example.ecommerce.entities.User;
+import com.example.ecommerce.repositories.OrderRepository;
 import com.example.ecommerce.repositories.UserRepository;
 
 @Configuration
@@ -16,15 +19,22 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User user1 = new User(-1, "João Rodrigues", "joao@gmail.com", "85999999999", "123");
-		User user2 = new User(-1, "Bruno Freitas", "bruno@gmail.com", "85999999991", "123");
+		User user1 = new User(null,"João Rodrigues", "joao@gmail.com", "85999999999", "123");
+		User user2 = new User(null,"Bruno Freitas", "bruno@gmail.com", "85999999991", "123");
+		
+		Order order1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), user1);
+		Order order2 = new Order(null, Instant.parse("2023-07-19T03:42:10Z"), user1);
+		Order order3 = new Order(null, Instant.parse("2023-07-20T21:21:22Z"), user2);
 		
 		userRepository.saveAll(Arrays.asList(user1,user2));
+		orderRepository.saveAll(Arrays.asList(order1,order2,order3));
 	}
-	
-	
+
 }
--- src/main/java/com/example/ecommerce/entities/User.java
+++ src/main/java/com/example/ecommerce/entities/User.java
@@ -1,12 +1,17 @@
 package com.example.ecommerce.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,16 +22,21 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -34,11 +44,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -72,6 +82,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
